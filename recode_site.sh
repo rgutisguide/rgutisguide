52218159975d40012a92cc80d1c359b3167a7ff7
vrud74 <vrud@mail.ru>
2020-11-23 01:07:33 +0300

    Add files via upload

---
+++ recode_site.sh
@@ -0,0 +1,6 @@
+iconv -f CP1251 -t UTF-8 index.html > index.htm
+iconv -f CP1251 -t UTF-8 about.html > about.htm
+iconv -f CP1251 -t UTF-8 Project1/page1.html > Project1/page1.htm
+iconv -f CP1251 -t UTF-8 Project2/page2.html > Project2/page2.htm
+iconv -f CP1251 -t UTF-8 Project3/page3.html > Project3/page3.htm
+iconv -f CP1251 -t UTF-8 Project4/page4.html > Project4/page4.htm
